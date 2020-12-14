06f29f7827e1802ed15f08148476305bcf4a301e
Lorenzo Magro <magrolorenzo@gmail.com>
2020-12-14 15:59:53 +0100

    Test php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    // $test = "test";
+    $name = $_GET["name"];
+?>
+
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Censuratore</title>
+    </head>
+    <body>
+        <h1>
+            Ciao mi chiamo
+            <?php
+                echo $name;
+            ?>
+        </h1>
+    </body>
+</html>
